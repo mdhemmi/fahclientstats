60e57a58cd05b9acca69119530c3527cb8f0ee90
hemmi <hemmi275@xmsoft.de>
2020-03-25 17:00:09 +0100

    implement timeout in fahquery.sh

--- fahquery.sh
+++ fahquery.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect -f
 
 log_user 0
-set timeout 60
+set timeout 5
 set prompt  ">"
 
 spawn -noecho telnet [lindex $argv 0] 36330
@@ -10,13 +10,14 @@ send "auth [lindex $argv 1]\r"
 expect $prompt
 send "queue-info\r"
 set slots {}
-set timout 5
+set timout 2
 expect {
 	-regexp {\{.*?\}} {
 		set slots "${slots}$expect_out(0,string)"
 		set slots "${slots}|"
 		exp_continue
 	}
+	timeout { }
 }
 puts $slots
 expect $prompt
